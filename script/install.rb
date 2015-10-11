0ea1b45e6c65811ea19d53b72888278c1c69094f
Bill Robinson <dseevr@users.noreply.github.com>
2015-10-11 10:52:11 +0800

    Added script/install.rb script to make sure all dotfiles are linked into $HOME

---
+++ script/install.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require "fileutils"
+
+BASE_DIR = File.expand_path("../..", __FILE__)
+HOME_DIR = ENV["HOME"]
+
+
+# Simple dotfiles can be symlinked into $HOME
+Dir.glob("#{BASE_DIR}/.*").each do |source_path|
+  filename = source_path.split("/").last
+
+  # skip current, parent, and git directories
+  next if %w[. .. .git].include?(filename)
+
+  target_path = "#{HOME_DIR}/#{filename}"
+
+  if File.exist?(target_path)
+    puts "WARNING: Skipping #{filename} because ~/#{filename} already exists"
+    next
+  end
+
+  FileUtils.ln_s(source_path, target_path)
+
+  puts "Linking #{filename} -> ~/#{filename}"
+end
+
+# TODO: directory-based linking
