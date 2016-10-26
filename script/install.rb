8ef602bf957ccd562eff77cc50ce48a345c4d8c5
Bill Robinson <dseevr@users.noreply.github.com>
2016-10-26 12:30:06 -0700

    added htoprc

--- script/install.rb
+++ script/install.rb
@@ -5,6 +5,8 @@ require "fileutils"
 BASE_DIR = File.expand_path("../..", __FILE__)
 HOME_DIR = ENV["HOME"]
 
+# TODO: replace all of this code with a simple array of [source, dest] pairs
+#       if dest has slashes in it, run FileUtils.mkdir_p first
 
 # Simple dotfiles can be symlinked into $HOME
 Dir.glob("#{BASE_DIR}/.*").each do |source_path|
@@ -25,4 +27,18 @@ Dir.glob("#{BASE_DIR}/.*").each do |source_path|
   puts "Linking #{filename} -> ~/#{filename}"
 end
 
-# TODO: directory-based linking
+# Configs with custom paths
+
+# htoprc
+htop_src = "#{BASE_DIR}/htoprc"
+htop_dir = "#{HOME_DIR}/.config/htop"
+htop_dest = htop_dir + "/htoprc"
+
+if File.exist?(htop_dest)
+  puts "WARNING: skipping htoprc because #{htop_dest} already exists"
+else
+  puts "Linking #{htop_src} -> #{htop_dest}"
+  FileUtils.mkdir_p(htop_dir)
+  FileUtils.ln_s(htop_src, htop_dest)
+end
+
